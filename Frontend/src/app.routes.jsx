2429ad8907747093b908c86a64e52c80fcc2bca3
Amaan0907 <mohdamaan00907@gmail.com>
2026-04-13 00:27:17 +0530

    Frontend Setup Done

---
+++ Frontend/src/app.routes.jsx
@@ -0,0 +1,13 @@
+import {createBrowserRouter} from "react-router"
+import Login from "../pages/Login.jsx"
+import Register from "../pages/Register.jsx"
+
+export const router=createBrowserRouter([
+    {
+        path:"/login",
+        element:<Login/>
+    },{
+        path:"/register",
+        element:<Register/>
+    }
+])
